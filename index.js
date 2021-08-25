0efd5782c41b4bf69c958e9c9717d6d8eaa8277e
R. Febriansyah <71978432+RFebrians@users.noreply.github.com>
2021-08-25 11:06:29 +0700

    Update

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
-const PORT = process.env.PORT || '3000'
+const app = express();
+const PORT = process.env.PORT || "3000";
 
-const booksRoute = require('./api/books')
-const hotelsRoute = require('./api/hotels')
+const booksRoute = require("./api/books");
+const hotelsRoute = require("./api/hotels");
 
 // Use middleware
-app.use( cors() )
-app.use( bodyParser.urlencoded({extended: false}) )
-app.use( bodyParser.json() )
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Handle http requests
-app.get('/', (req, res) => { return res.send('The API is up and running!') })
+app.get("/", (req, res) => {
+  return res.send("Requesting API On Standby");
+});
 
 // Add additional routes
-app.use('/books', booksRoute)
-app.use('/hotels', hotelsRoute)
+app.use("/books", booksRoute);
+app.use("/hotels", hotelsRoute);
 
 // Start the server
-app.listen(PORT, () => console.log(`Running on port ${PORT}`) )
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
 
-exports = module.exports = app
+exports = module.exports = app;
